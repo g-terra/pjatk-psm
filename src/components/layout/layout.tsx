a1ef877383f48d3a2563adb3aef3af9f3b495864
Guilherme Terra <by.terra@hotmail.com>
2023-04-22 22:13:18 +0200

    fixing build

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,14 @@
 // wrapper around the main content of the page including padding
 // and the main content of the page
 
-import React from 'react';
+import React, {ReactNode} from 'react';
 
+type LayoutProps = {
+    children: ReactNode;
+};
 
-export default function Layout({ children }) {
-
-return (
+export default function Layout({ children }: LayoutProps) {
+    return (
         <div className="min-h-screen bg-gray-100">
             <div className="max-w-7xl mx-auto py-6 md:px-32">
                 {children}
